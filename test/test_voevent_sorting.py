088a9ce30e6ea69705869219b81937f1b995513b
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-11 16:40:32 +0100

    deleted useless code

--- test/test_voevent_sorting.py
+++ test/test_voevent_sorting.py
@@ -1,5 +1,4 @@
 from test_voevents import DUMMY_VOEVENT_GCN, DUMMY_VOEVENT_INTEGRAL, DUMMY_VOEVENT_CHIME, DUMMY_VOEVENT_LIGO, DUMMY_VOEVENT_LIGO_INITIAL, DUMMY_VOEVENT_LIGO_PRELIMINARY, DUMMY_VOEVENT_GCN_FERMI, DUMMY_VOEVENT_GCN_MAXI, DUMMY_VOEVENT_AGILE
-from xml import xml_document
 import voeventparse as vp
 from comet.plugins.voeventsorting import VoeventSorting
 
@@ -7,27 +6,27 @@ class DummyEvent(object):
     """
     Class containing standard voevent from different networks
     """
-    gcn = xml_document(DUMMY_VOEVENT_GCN)
-    chime = xml_document(DUMMY_VOEVENT_CHIME)
-    integral = xml_document(DUMMY_VOEVENT_INTEGRAL)
-    fermi = xml_document(DUMMY_VOEVENT_GCN_FERMI) 
-    ligo = xml_document(DUMMY_VOEVENT_LIGO)
-    ligo2 = xml_document(DUMMY_VOEVENT_LIGO_PRELIMINARY)
-    ligo_initial = xml_document(DUMMY_VOEVENT_LIGO_INITIAL)
-    maxi = xml_document(DUMMY_VOEVENT_GCN_MAXI)
-    agile = xml_document(DUMMY_VOEVENT_AGILE)
+    gcn = DUMMY_VOEVENT_GCN
+    chime = DUMMY_VOEVENT_CHIME
+    integral = DUMMY_VOEVENT_INTEGRAL
+    fermi = DUMMY_VOEVENT_GCN_FERMI
+    ligo = DUMMY_VOEVENT_LIGO
+    ligo2 = DUMMY_VOEVENT_LIGO_PRELIMINARY
+    ligo_initial = DUMMY_VOEVENT_LIGO_INITIAL
+    maxi = DUMMY_VOEVENT_GCN_MAXI
+    agile = DUMMY_VOEVENT_AGILE
 
 if __name__ == "__main__":
     dummyevents = DummyEvent()
-    voe_chime = vp.loads(dummyevents.chime.raw_bytes) #tested
-    voe_gcn = vp.loads(dummyevents.gcn.raw_bytes) #tested
-    voe_integral = vp.loads(dummyevents.integral.raw_bytes) #tested
-    voe_fermi = vp.loads(dummyevents.fermi.raw_bytes) #tested
-    voe_ligo = vp.loads(dummyevents.ligo.raw_bytes) #tested
-    voe_ligo_2 = vp.loads(dummyevents.ligo2.raw_bytes) #tested
-    voe_ligo_init = vp.loads(dummyevents.ligo_initial.raw_bytes) #tested
-    voe_maxi = vp.loads(dummyevents.maxi.raw_bytes) #tested
-    voe_agile = vp.loads(dummyevents.agile.raw_bytes) 
+    voe_chime = vp.loads(dummyevents.chime) #tested
+    voe_gcn = vp.loads(dummyevents.gcn) #tested
+    voe_integral = vp.loads(dummyevents.integral) #tested
+    voe_fermi = vp.loads(dummyevents.fermi) #tested
+    voe_ligo = vp.loads(dummyevents.ligo) #tested
+    voe_ligo_2 = vp.loads(dummyevents.ligo2) #tested
+    voe_ligo_init = vp.loads(dummyevents.ligo_initial) #tested
+    voe_maxi = vp.loads(dummyevents.maxi) #tested
+    voe_agile = vp.loads(dummyevents.agile) #tested
     
     voe_sorter = VoeventSorting()
     print(voe_sorter.sort(voe_chime))
