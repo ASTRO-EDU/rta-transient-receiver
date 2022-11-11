57f60affde669115b6de9a1d988f82c0bd3bbac4
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-11 15:22:02 +0100

    added important event function and changed email body message

--- emailnotifier.py
+++ emailnotifier.py
@@ -11,23 +11,27 @@ class EmailNotifier(object):
         self.mail = Mail(config["sender_email"], config["sender_email_password"])
         self.to = config["email_recivers"]
         self.packet_with_email_notification = config["packet_with_email_notification"]
+        self.important_email_subject = config["important_email_subject"]
 
     def sendEmails(self, voeventdata, result_row):
         
         if voeventdata.packet_type in self.packet_with_email_notification or voeventdata.is_ste: #it will send an email for GW, Neutrino event or STE events
             subject = f'Notice alert for {voeventdata.name}'
-            body = f'The AFIS platform received a notice for the {voeventdata.name} event at {voeventdata.UTC} for triggerID {voeventdata.trigger_id} {chr(10)} available at \
-            http://afiss.iasfbo.inaf.it/afiss/full_results.html?instrument_name={voeventdata.name}&trigger_time_utc={voeventdata.UTC}&trigger_id={Utils.graceID_from_triggerId(voeventdata.name, voeventdata.trigger_id)}&seqnum={voeventdata.seqNum}'
-
+            body = f'The platform received a notice for the {voeventdata.name} event at {voeventdata.UTC} for triggerID {voeventdata.trigger_id}'
             self.mail.send_email(self.to, subject, body)
         
         #check if there are correlated instruments and send an email if so
         if result_row:
             subject = f'Correlations for {voeventdata.name}'
-            body = f'The AFIS platform received a notice for the {voeventdata.name} at {voeventdata.UTC} for triggerID {voeventdata.trigger_id} {chr(10)} available at \
-            http://afiss.iasfbo.inaf.it/afiss/full_results.html?instrument_name={voeventdata.name}&trigger_time_utc={voeventdata.UTC}&trigger_id={Utils.graceID_from_triggerId(voeventdata.name, voeventdata.trigger_id)}&seqnum={voeventdata.seqNum} {chr(10)} with the following correlated events: {chr(10)} {str(result_row)}'
+            body = f'The platform received a notice for the {voeventdata.name} at {voeventdata.UTC} for triggerID {voeventdata.trigger_id} with the following correlated events: {str(result_row)}'
 
-            self.mail.send_email(self.to, subject, body)
+        if self.is_important(voeventdata):
+            subject = self.important_email_subject + " " + subject
+
+        self.mail.send_email(self.to, subject, body)
+
+    def is_important(self, voeventdata) -> bool:
+        return False
 
 class Mail():
     
