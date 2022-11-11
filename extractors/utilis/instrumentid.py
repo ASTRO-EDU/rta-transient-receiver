56ce7d2e1c93d933313331bd0ac96cb69f284c01
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-11 09:59:08 +0100

    created enum for map instrument name to the corresponding instrumentid

---
+++ extractors/utilis/instrumentid.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Instrument(Enum):
+    TEST = 0
+    FERMI_GBM = 1
+    FERMI_LAT = 2
+    SWIFT = 3
+    AGILE_GRID = 4
+    AGILE_MCAL = 5
+    AGILE_SA = 6
+    LIGO = 7
+    ICECUBE_HESE = 8
+    AGILE = 9
+    ICECUBE_EHE = 10
+    AGILE_MCAL_AU  = 17
+    LIGO_TEST = 19
+    FRB = 20
+    ICECUBE_ASTROTRACK_GOLD = 21
+    ICECUBE_ASTROTRACK_BRONZE = 22
