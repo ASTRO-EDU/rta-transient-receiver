185f2fa06720fd9ecd91ef41cb2d96b98e1059e9
Luca Babboni <luca.babboni2@studio.unibo.it>
2022-11-14 10:39:31 +0100

    changed setup packages

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
+
 setup(
-    name="voevent-handler",
+    name="voeventhandler",
     author="Antonio Addis, Luca Babboni",
     version="2.0.0",
-    packages=['./', 'extractors/', 'extractors/utilis', 'test'],
+    packages=['voeventhandler/', 'voeventhandler/extractors/' ,'voeventhandler/utilis', 'voeventhandler/test'],
     data_files=[('voevent_handler_config', ['./config.json'])],
     license='GPL-3.0'
 )
